38382fe9457b850db760acb6e2f5b40a415433a1
cheigba <92983645+cheigba@users.noreply.github.com>
2021-10-23 21:24:55 +0800

    Add files via upload
    added grading system

---
+++ GradingSystem/GradingSystem/Form1.Designer.vb
@@ -0,0 +1,513 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.cboGrade = New System.Windows.Forms.ComboBox()
+        Me.cboSection = New System.Windows.Forms.ComboBox()
+        Me.txtSname = New System.Windows.Forms.TextBox()
+        Me.txtSubject = New System.Windows.Forms.TextBox()
+        Me.txtW1 = New System.Windows.Forms.TextBox()
+        Me.txtW2 = New System.Windows.Forms.TextBox()
+        Me.txtW3 = New System.Windows.Forms.TextBox()
+        Me.txtPT1 = New System.Windows.Forms.TextBox()
+        Me.txtPT2 = New System.Windows.Forms.TextBox()
+        Me.txtPT3 = New System.Windows.Forms.TextBox()
+        Me.txtQexam = New System.Windows.Forms.TextBox()
+        Me.btnCgrade = New System.Windows.Forms.Button()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.lblGrade = New System.Windows.Forms.Label()
+        Me.lblSection = New System.Windows.Forms.Label()
+        Me.lblSname = New System.Windows.Forms.Label()
+        Me.lblSubject = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Label18 = New System.Windows.Forms.Label()
+        Me.Label17 = New System.Windows.Forms.Label()
+        Me.Label16 = New System.Windows.Forms.Label()
+        Me.Label15 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(28, 23)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(85, 13)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Student's Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(28, 51)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Grade:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(303, 51)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(46, 13)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Section:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(31, 78)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(46, 13)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Subject:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(116, 135)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(78, 13)
+        Me.Label5.TabIndex = 4
+        Me.Label5.Text = "Written Works:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(313, 135)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(97, 13)
+        Me.Label6.TabIndex = 5
+        Me.Label6.Text = "Performance Task:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(75, 163)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(24, 13)
+        Me.Label7.TabIndex = 6
+        Me.Label7.Text = "W1"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(75, 189)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(24, 13)
+        Me.Label8.TabIndex = 7
+        Me.Label8.Text = "W3"
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(75, 215)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(24, 13)
+        Me.Label9.TabIndex = 8
+        Me.Label9.Text = "W3"
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(283, 163)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(27, 13)
+        Me.Label10.TabIndex = 9
+        Me.Label10.Text = "PT1"
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(283, 189)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(27, 13)
+        Me.Label11.TabIndex = 10
+        Me.Label11.Text = "PT2"
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(283, 215)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(27, 13)
+        Me.Label12.TabIndex = 11
+        Me.Label12.Text = "PT3"
+        '
+        'cboGrade
+        '
+        Me.cboGrade.FormattingEnabled = True
+        Me.cboGrade.Items.AddRange(New Object() {"11", "12"})
+        Me.cboGrade.Location = New System.Drawing.Point(73, 48)
+        Me.cboGrade.Name = "cboGrade"
+        Me.cboGrade.Size = New System.Drawing.Size(121, 21)
+        Me.cboGrade.TabIndex = 12
+        '
+        'cboSection
+        '
+        Me.cboSection.FormattingEnabled = True
+        Me.cboSection.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6", "7"})
+        Me.cboSection.Location = New System.Drawing.Point(355, 48)
+        Me.cboSection.Name = "cboSection"
+        Me.cboSection.Size = New System.Drawing.Size(121, 21)
+        Me.cboSection.TabIndex = 13
+        '
+        'txtSname
+        '
+        Me.txtSname.Location = New System.Drawing.Point(119, 20)
+        Me.txtSname.Name = "txtSname"
+        Me.txtSname.Size = New System.Drawing.Size(230, 20)
+        Me.txtSname.TabIndex = 14
+        '
+        'txtSubject
+        '
+        Me.txtSubject.Location = New System.Drawing.Point(83, 75)
+        Me.txtSubject.Name = "txtSubject"
+        Me.txtSubject.Size = New System.Drawing.Size(100, 20)
+        Me.txtSubject.TabIndex = 15
+        '
+        'txtW1
+        '
+        Me.txtW1.Location = New System.Drawing.Point(105, 160)
+        Me.txtW1.Name = "txtW1"
+        Me.txtW1.Size = New System.Drawing.Size(100, 20)
+        Me.txtW1.TabIndex = 17
+        '
+        'txtW2
+        '
+        Me.txtW2.Location = New System.Drawing.Point(105, 186)
+        Me.txtW2.Name = "txtW2"
+        Me.txtW2.Size = New System.Drawing.Size(100, 20)
+        Me.txtW2.TabIndex = 18
+        '
+        'txtW3
+        '
+        Me.txtW3.Location = New System.Drawing.Point(105, 212)
+        Me.txtW3.Name = "txtW3"
+        Me.txtW3.Size = New System.Drawing.Size(100, 20)
+        Me.txtW3.TabIndex = 19
+        '
+        'txtPT1
+        '
+        Me.txtPT1.Location = New System.Drawing.Point(316, 160)
+        Me.txtPT1.Name = "txtPT1"
+        Me.txtPT1.Size = New System.Drawing.Size(100, 20)
+        Me.txtPT1.TabIndex = 20
+        '
+        'txtPT2
+        '
+        Me.txtPT2.Location = New System.Drawing.Point(316, 186)
+        Me.txtPT2.Name = "txtPT2"
+        Me.txtPT2.Size = New System.Drawing.Size(100, 20)
+        Me.txtPT2.TabIndex = 21
+        '
+        'txtPT3
+        '
+        Me.txtPT3.Location = New System.Drawing.Point(316, 212)
+        Me.txtPT3.Name = "txtPT3"
+        Me.txtPT3.Size = New System.Drawing.Size(100, 20)
+        Me.txtPT3.TabIndex = 22
+        '
+        'txtQexam
+        '
+        Me.txtQexam.Location = New System.Drawing.Point(210, 296)
+        Me.txtQexam.Name = "txtQexam"
+        Me.txtQexam.Size = New System.Drawing.Size(100, 20)
+        Me.txtQexam.TabIndex = 23
+        '
+        'btnCgrade
+        '
+        Me.btnCgrade.Location = New System.Drawing.Point(401, 286)
+        Me.btnCgrade.Name = "btnCgrade"
+        Me.btnCgrade.Size = New System.Drawing.Size(75, 38)
+        Me.btnCgrade.TabIndex = 24
+        Me.btnCgrade.Text = "Compute Grade"
+        Me.btnCgrade.UseVisualStyleBackColor = True
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(123, 299)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(81, 13)
+        Me.Label13.TabIndex = 25
+        Me.Label13.Text = "Quarterly Exam:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.lblGrade)
+        Me.GroupBox1.Controls.Add(Me.lblSection)
+        Me.GroupBox1.Controls.Add(Me.lblSname)
+        Me.GroupBox1.Controls.Add(Me.lblSubject)
+        Me.GroupBox1.Controls.Add(Me.PictureBox1)
+        Me.GroupBox1.Controls.Add(Me.Label18)
+        Me.GroupBox1.Controls.Add(Me.Label17)
+        Me.GroupBox1.Controls.Add(Me.Label16)
+        Me.GroupBox1.Controls.Add(Me.Label15)
+        Me.GroupBox1.Controls.Add(Me.Label14)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 372)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(554, 175)
+        Me.GroupBox1.TabIndex = 26
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "GroupBox1"
+        '
+        'lblGrade
+        '
+        Me.lblGrade.AccessibleDescription = ""
+        Me.lblGrade.AutoSize = True
+        Me.lblGrade.Location = New System.Drawing.Point(92, 144)
+        Me.lblGrade.Name = "lblGrade"
+        Me.lblGrade.Size = New System.Drawing.Size(0, 13)
+        Me.lblGrade.TabIndex = 9
+        '
+        'lblSection
+        '
+        Me.lblSection.AutoSize = True
+        Me.lblSection.Location = New System.Drawing.Point(92, 117)
+        Me.lblSection.Name = "lblSection"
+        Me.lblSection.Size = New System.Drawing.Size(0, 13)
+        Me.lblSection.TabIndex = 8
+        '
+        'lblSname
+        '
+        Me.lblSname.AutoSize = True
+        Me.lblSname.Location = New System.Drawing.Point(92, 88)
+        Me.lblSname.Name = "lblSname"
+        Me.lblSname.Size = New System.Drawing.Size(0, 13)
+        Me.lblSname.TabIndex = 7
+        '
+        'lblSubject
+        '
+        Me.lblSubject.AutoSize = True
+        Me.lblSubject.Location = New System.Drawing.Point(234, 58)
+        Me.lblSubject.Name = "lblSubject"
+        Me.lblSubject.Size = New System.Drawing.Size(0, 13)
+        Me.lblSubject.TabIndex = 6
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(61, 16)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(46, 32)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 5
+        Me.PictureBox1.TabStop = False
+        '
+        'Label18
+        '
+        Me.Label18.AutoSize = True
+        Me.Label18.Location = New System.Drawing.Point(-7, 144)
+        Me.Label18.Name = "Label18"
+        Me.Label18.Size = New System.Drawing.Size(90, 13)
+        Me.Label18.TabIndex = 4
+        Me.Label18.Text = "  Quarterly Grade:"
+        '
+        'Label17
+        '
+        Me.Label17.AutoSize = True
+        Me.Label17.Location = New System.Drawing.Point(2, 117)
+        Me.Label17.Name = "Label17"
+        Me.Label17.Size = New System.Drawing.Size(81, 13)
+        Me.Label17.TabIndex = 3
+        Me.Label17.Text = "Grade & Section:"
+        '
+        'Label16
+        '
+        Me.Label16.AutoSize = True
+        Me.Label16.Location = New System.Drawing.Point(2, 88)
+        Me.Label16.Name = "Label16"
+        Me.Label16.Size = New System.Drawing.Size(85, 13)
+        Me.Label16.TabIndex = 2
+        Me.Label16.Text = "Student's Name:"
+        '
+        'Label15
+        '
+        Me.Label15.AutoSize = True
+        Me.Label15.Location = New System.Drawing.Point(158, 58)
+        Me.Label15.Name = "Label15"
+        Me.Label15.Size = New System.Drawing.Size(70, 13)
+        Me.Label15.TabIndex = 1
+        Me.Label15.Text = "Grade  Slip in"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(111, 23)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(303, 13)
+        Me.Label14.TabIndex = 0
+        Me.Label14.Text = "Dr. Filemon C. Aguilar Information Technology Training Institute"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(578, 340)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label13)
+        Me.Controls.Add(Me.btnCgrade)
+        Me.Controls.Add(Me.txtQexam)
+        Me.Controls.Add(Me.txtPT3)
+        Me.Controls.Add(Me.txtPT2)
+        Me.Controls.Add(Me.txtPT1)
+        Me.Controls.Add(Me.txtW3)
+        Me.Controls.Add(Me.txtW2)
+        Me.Controls.Add(Me.txtW1)
+        Me.Controls.Add(Me.txtSubject)
+        Me.Controls.Add(Me.txtSname)
+        Me.Controls.Add(Me.cboSection)
+        Me.Controls.Add(Me.cboGrade)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Grading System"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+
+    Private Sub Label9_Click(sender As Object, e As EventArgs) Handles Label9.Click
+
+    End Sub
+    Private Sub Label8_Click(sender As Object, e As EventArgs) Handles Label8.Click
+
+    End Sub
+    Friend WithEvents cboGrade As System.Windows.Forms.ComboBox
+    Friend WithEvents cboSection As System.Windows.Forms.ComboBox
+    Friend WithEvents txtSname As System.Windows.Forms.TextBox
+    Friend WithEvents txtSubject As System.Windows.Forms.TextBox
+    Friend WithEvents txtW1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtW2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtW3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtPT1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtPT2 As System.Windows.Forms.TextBox
+    Friend WithEvents txtPT3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtQexam As System.Windows.Forms.TextBox
+    Friend WithEvents btnCgrade As System.Windows.Forms.Button
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles Label2.Click
+
+    End Sub
+
+    Private Sub Label5_Click(sender As Object, e As EventArgs) Handles Label5.Click
+
+    End Sub
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label18 As System.Windows.Forms.Label
+    Friend WithEvents Label17 As System.Windows.Forms.Label
+    Friend WithEvents Label16 As System.Windows.Forms.Label
+    Friend WithEvents Label15 As System.Windows.Forms.Label
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents lblGrade As System.Windows.Forms.Label
+    Friend WithEvents lblSection As System.Windows.Forms.Label
+    Friend WithEvents lblSname As System.Windows.Forms.Label
+    Friend WithEvents lblSubject As System.Windows.Forms.Label
+
+    Private Sub txt_TextChanged(sender As Object, e As EventArgs) Handles txtQexam.TextChanged
+
+    End Sub
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+
+
+
+
+    End Sub
+
+    Private Sub btnCgrade_ControlRemoved(sender As Object, e As ControlEventArgs) Handles btnCgrade.ControlRemoved
+
+    End Sub
+
+    Private Sub btnCgrade_CursorChanged(sender As Object, e As EventArgs) Handles btnCgrade.CursorChanged
+
+    End Sub
+
+    Private Sub btnCgrade_Click(sender As Object, e As EventArgs) Handles btnCgrade.Click
+        Dim ww, pt, qe, grade As Single
+
+        ww = (Val(Me.txtW1.Text) + Val(Me.txtW2.Text) + Val(Me.txtW3.Text)) / 3
+        pt = (Val(Me.txtPT1.Text) + Val(Me.txtPT2.Text) + Val(Me.txtPT3.Text)) / 3
+        qe = Val(Me.txtQexam.Text)
+        grade = ww * 0.25 + pt * 0.5 + qe * 0.25
+
+        Me.Height = 586
+        Me.lblSubject.Text = Me.txtSubject.Text
+        Me.lblSection.Text = "Grade" + Me.cboGrade.Text + "" + "Section" + Me.cboSection.Text
+        Me.lblSname.Text = Me.txtSname.Text
+        Me.lblGrade.Text = grade
+
+
+
+
+    End Sub
+End Class
---
+++ GradingSystem/GradingSystem/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following 
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("GradingSystem")> 
+<Assembly: AssemblyDescription("")> 
+<Assembly: AssemblyCompany("")> 
+<Assembly: AssemblyProduct("GradingSystem")> 
+<Assembly: AssemblyCopyright("Copyright ©  2017")> 
+<Assembly: AssemblyTrademark("")> 
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("999c6aab-d19e-40dc-b60e-af8a2a91d1bf")> 
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version 
+'      Build Number
+'      Revision
+'
+' You can specify all the values or you can default the Build and Revision Numbers 
+' by using the '*' as shown below:
+' <Assembly: AssemblyVersion("1.0.*")> 
+
+<Assembly: AssemblyVersion("1.0.0.0")> 
+<Assembly: AssemblyFileVersion("1.0.0.0")> 
