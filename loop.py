d9066351192b69baceeb57a44b50c832bf853fb5
Paras Saini <116185291+Paras-code@users.noreply.github.com>
2022-10-30 16:50:10 +0530

    Create loop.py
    #7

---
+++ loop.py
@@ -0,0 +1,16 @@
+i = 0
+while(i<=10):
+    print(i)
+    i = i + 1
+
+
+dic = {
+    "Name" : "Avaneesh",
+    "Course":"Modern genetics",
+    "Marks" : 86,
+    "Credit hours" : 4
+
+}
+for item in dic:
+    print(item)
+
